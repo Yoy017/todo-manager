bf5b665fd44bc7005a5cdf2c30a1014a67789ab3
Camille Theubet <camille.theubet@hes-so.ch>
2024-10-17 00:26:44 +0200

    Started to split the commands in multiple files

---
+++ src/main/java/ch/heigvd/dai/commands/Create.java
@@ -0,0 +1,45 @@
+package ch.heigvd.dai.commands;
+
+import ch.heigvd.dai.util.Status;
+import ch.heigvd.dai.util.Task;
+import ch.heigvd.dai.util.fileWriter;
+import picocli.CommandLine;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "create", description = "Create a task in a file")
+public class Create implements Callable<Integer> {
+    @CommandLine.ParentCommand protected Root parent;
+
+    @CommandLine.Option(names = {"-t", "--title"}, description = "Title of the task")
+    private String title;
+
+    @CommandLine.Option(names = {"-d", "--description"}, description = "Description of the task")
+    private String description;
+
+    @CommandLine.Option(names = {"-s", "--status"}, description = "Select a task by status [PENDING, IN_PROGRESS, DONE]")
+    private Status state;
+
+    @Override
+    public Integer call() {
+        Task task;
+        if(title == null)
+            task = new Task();
+        else if(description == null && state == null)
+            task = new Task(title);
+        else if(description != null && state == null)
+            task = new Task(title, description);
+        else
+            task = new Task(title, description, state);
+
+            fileWriter fw = new fileWriter(parent.filename);
+        try{
+            fw.writeFile(task, true);
+            System.out.println("Task successfully created.");
+        } catch (FileNotFoundException e) {
+            System.out.println("You can only create tasks in an existing file.");
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/ch/heigvd/dai/commands/ShowTasks.java
@@ -0,0 +1,50 @@
+package ch.heigvd.dai.commands;
+
+import ch.heigvd.dai.commands.Root;
+import ch.heigvd.dai.util.Status;
+import ch.heigvd.dai.util.SubTask;
+import ch.heigvd.dai.util.Task;
+import ch.heigvd.dai.util.fileReader;
+import picocli.CommandLine;
+
+import java.io.FileNotFoundException;
+import java.util.Vector;
+import java.util.concurrent.Callable;
+
+
+
+
+@CommandLine.Command(name = "show", description = "Display all created tasks")
+public class ShowTasks implements Callable<Integer> {
+    @CommandLine.ParentCommand protected Root parent;
+
+    @CommandLine.Option(names = {"-s", "--status"}, description = "Select a task by status [PENDING, IN_PROGRESS, DONE]")
+    private Status state;
+
+    @Override
+    public Integer call() {
+        fileReader fi = new fileReader(parent.filename);
+        Vector<Task> tasks;
+        try {
+            tasks = fi.getAllTask();
+        } catch (FileNotFoundException e) {
+            System.out.println("You can only show tasks in an existing file.");
+            return 1;
+        }
+
+        if(tasks.isEmpty()) {
+            System.err.println("No task created yet.");
+            return 0;
+        }
+
+        for(int i = 0; i < tasks.size(); ++i) {
+            if(tasks.elementAt(i).state == state || state == null) {
+                System.out.println(tasks.elementAt(i)); // Affiche la tâche
+                for (SubTask subTask : tasks.elementAt(i).getSubTasks()) {
+                    System.out.println(subTask); // Affiche des sous-tâches
+                }
+            }
+        }
+        return 0;
+    }
+}
