588f844b2e6d14c4dc64fa4174d9a2974942a016
Camille Theubet <camille.theubet@hes-so.ch>
2024-10-17 09:57:52 +0200

    Ready to merge

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,12 +1,14 @@
 package ch.heigvd.dai;
 
 import picocli.CommandLine;
-import ch.heigvd.dai.commands.TaskManager;
+//import ch.heigvd.dai.commands.TaskManager;
+import ch.heigvd.dai.commands.Root;
 
 public class Main {
 
     public static void main(String[] args) {
         // Crée la commande principale 'list' et ses sous-commandes
+        /*
         CommandLine cmd = new CommandLine(new TaskManager())
                 .addSubcommand("create", new TaskManager.CreateTask())
                 .addSubcommand("show", new TaskManager.ShowTasks())
@@ -14,6 +16,16 @@ public class Main {
                 .addSubcommand("update", new TaskManager.updateTask())
                 .addSubcommand("subtask", new TaskManager.AddSubTask());
 
-        cmd.execute(args); // Exécute la commande avec les arguments passés
+         */
+
+        //cmd.execute(args); // Exécute la commande avec les arguments passés
+        int exitCode =
+                new CommandLine(new Root())
+                        .execute(args);
+        Long end = System.nanoTime();
+
+
+        System.exit(exitCode);
     }
+
 }
